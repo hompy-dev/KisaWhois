b1db624fd540bfc2b91db9d7c7c6f3ded02a1ca7
hompy <contact@hompy.dev>
2023-12-15 22:02:29 +0900

    Update KisaWhois.php

--- KisaWhois.php
+++ KisaWhois.php
@@ -10,51 +10,56 @@
 
 declare(strict_types=1);
 
+namespace HompyDev\OpenApi;
+
 class KisaWhois
 {
     // 인증키
-    private const SERVICE_KEY = '';
+    private const SERVICE_KEY = ''; // 유효한 인증키로 변경
 
     // 서비스 URL
     private const SERVICE_URL = 'https://apis.data.go.kr/B551505/whois';
 
     // 응답형식 (json|xml)
-    protected static string $answer = 'json';
+    private const ANSWER = 'json';
 
     // 요청주소
-    private static array $endPoint = [
-        'domain'  => KisaWhois::SERVICE_URL . '/domain_name',
-        'ip'      => KisaWhois::SERVICE_URL . '/ip_address',
-        'asn'     => KisaWhois::SERVICE_URL . '/as_number',
-        'country' => KisaWhois::SERVICE_URL . '/ipas_country_code'
+    private const ENDPOINTS = [
+        'domain'  => self::SERVICE_URL . '/domain_name',
+        'ip'      => self::SERVICE_URL . '/ip_address',
+        'asn'     => self::SERVICE_URL . '/as_number',
+        'country' => self::SERVICE_URL . '/ipas_country_code'
     ];
 
-    public function __construct()
-    {
-
-    }
-
-    public static function lookup(string $query, string $mode = 'ip')
+    public function lookup(string $query, string $mode = 'ip')
     {
         $params = [
-            'serviceKey' => KisaWhois::SERVICE_KEY,
+            'serviceKey' => self::SERVICE_KEY,
             'query' => $query,
-            'answer' => self::$answer
+            'answer' => self::ANSWER
         ];
-        $url = self::$endPoint[$mode] . '?' . http_build_query($params);
-        return self::request($url);
+        $url = self::ENDPOINTS[$mode] . '?' . http_build_query($params);
+        return $this->request($url);
     }
 
-    protected static function request($url)
+    protected function request($url)
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-        $response = curl_exec($ch);
-        curl_close($ch);
+        try {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+            $response = curl_exec($ch);
 
-        return $response;
+            if ($response === false) {
+                throw new \Exception('cURL request failed: ' . curl_error($ch));
+            }
+            curl_close($ch);
+            return $response;
+        }
+        catch (\Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
     }
 }
